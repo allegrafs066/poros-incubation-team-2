2fdc6a924aff51f6f87ef5a912a175ff457e10a5
Allegra Fernanda Santoso <93397084+allegrafs066@users.noreply.github.com>
2024-03-20 13:39:46 +0700

    Updated Cart's Children behavior

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,12 @@
 import NavBar from "../components/NavBar";
+import style from "./MainLayout.module.css"; // Import CSS module for MainLayout styling
 
 function MainLayout(props) {
   return (
-    <>
+    <div className={style.mainLayout}>
       <NavBar />
-      {props.children}
-    </>
+      <div className={style.content}>{props.children}</div>
+    </div>
   );
 }
 
--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,16 +1,33 @@
 import { useContext } from "react";
 import MainLayout from "../../layouts/MainLayout";
 import GlobalContext from "../../context/GlobalContext";
+import style from "./Cart.module.css";
 
 function CartPage() {
   const { cartProducts } = useContext(GlobalContext);
 
   return (
     <MainLayout>
-      <main>
-        {cartProducts.map((product) => {
-          return <div key={product.id}>{product.title}</div>;
-        })}
+      <main className={style.cartContainer}>
+        {cartProducts.length > 0 ? (
+          cartProducts.map((product) => (
+            <div key={product.id} className={style.cartItem}>
+              <img
+                src={product.image}
+                alt={product.title}
+                className={style.cartItemImage}
+              />
+              <div className={style.cartItemInfo}>
+                <div className={style.cartItemTitle}>{product.title}</div>
+                <div className={style.cartItemPrice}>${product.price}</div>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div className={style.emptyCartMessage}>
+            {"Let's go add something in your cart!"}
+          </div>
+        )}
       </main>
     </MainLayout>
   );
